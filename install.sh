e10db9cc09f0a757c547119782450e2ba352c29e
yongholeeme <inwalter99@gmail.com>
2022-12-30 15:59:50 +0900

    Add install script for applications

---
+++ install.sh
@@ -0,0 +1,14 @@
+# [Homebrew]
+/bin/bash -c '$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)'
+
+# [NVM]
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+
+# [Starship]
+curl -sS https://starship.rs/install.sh | sh
+
+# [Warp]
+brew install --cask warp
+
+# [Raycast]
+brew install --cask raycast
