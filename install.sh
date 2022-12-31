e7fb6397b687be169bb3d90a222a306334083ee3
yongholeeme <inwalter99@gmail.com>
2022-12-31 11:10:04 +0900

    Change the order of install

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 # [Homebrew]
-/bin/bash -c '$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)'
+curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
 
 # [NVM]
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 
-# [Starship]
-curl -sS https://starship.rs/install.sh | sh
-
 # [Warp]
 brew install --cask warp
+
+# [Starship]
+curl -sS https://starship.rs/install.sh | sh
 
 # [Raycast]
 brew install --cask raycast
