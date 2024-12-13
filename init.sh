b1e666e4f9b2135a8bf2cf803c30de739b459bad
Yongho Lee <yongholeeme@gmail.com>
2024-12-13 17:07:08 +0900

    ✨ Update nvm version

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 
 echo '### this is overwrited by config/init.sh
 export CONFIG="$HOME/.config"
